473500d8ebf6d894b30acfc5ca7a6d78b4bc1d14
Cami Anderson <camidantas@icloud.com>
2022-09-20 09:06:46 -0400

    Add news to NewsList

--- src/components/NewsList/NewsList.js
+++ src/components/NewsList/NewsList.js
@@ -1,10 +1,26 @@
 import React from "react";
+import './NewsList.css';
 
-const NewsList = () => {
-
+const NewsList = ({newsData}) => {
+    console.log(newsData)
+    const news = newsData.map((news)=> {
+        if(news.title === ''|| news.title === 'Sign Up for the Science Times Newsletter') {
+            return null
+        }
+        return (
+            <section className="news_section" key={news.created_date}>
+                <img src={news.multimedia[1].url} className='news_section-img'/>
+                <h2>{news.title}</h2>
+                <p>{news.published_date}</p>
+                <p>{news.section}</p>
+                <p>{news.abstract}</p>
+                <button>Read more</button>
+            </section>
+        )
+    })
 
     return (
-        <h1>News List</h1>
+        <div className="news">{news}</div>
     )
 }
 
