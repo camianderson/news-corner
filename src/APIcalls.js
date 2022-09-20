3def10f61977814ebff20a95a28c0e5411911223
Cami Anderson <camidantas@icloud.com>
2022-09-20 10:26:32 -0400

    Add dropdown options

--- src/APIcalls.js
+++ src/APIcalls.js
@@ -1,5 +1,5 @@
-const getNews = () => {
-    return fetch('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=Z4xsGo3Ks6YkKjPUoWXwo5HbFtPlIwfT')
+const getNews = (topic) => {
+    return fetch(`https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=Z4xsGo3Ks6YkKjPUoWXwo5HbFtPlIwfT`)
     .then(response => response.json())
 }
 
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,52 @@
 import './App.css';
 import NewsList from '../NewsList/NewsList';
 import Article from '../Article/Article';
+// import Home from '../Home/Home'
 import { getNews } from '../../APIcalls'
 import { useEffect, useState } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
 
   const [newsData, setNewsData] = useState([])
 
-  const getNewsData = () => {
-    getNews()
+  const handleChange = event => {
+    getNewsData(event.target.value)
+  }
+
+  const getNewsData = (value) => {
+    getNews(value)
     .then(data => setNewsData(data.results))
   }
 
-  useEffect(() => {
-    getNewsData();
+  useEffect((value) => {
+      getNewsData('us');
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <h1> News Feed </h1>
+        <div className="home">
+            <select onChange={handleChange}>
+                <option value="us">U.S.</option>
+                <option value="world">World</option>
+                <option value="arts">Arts</option>
+                <option value="automobiles">Automobiles</option>
+                <option value="business">Business</option>
+                <option value="fashion">Fashion</option>
+                <option value="health">Health</option>
+                <option value="science">Science</option>
+                <option value="sports">Sports</option>
+                <option value="technology">Technology</option>
+                <option value="travel">Travel</option>
+            </select>
+        </div>
       </header>
-      <Route exact path='/' render={() => <NewsList newsData={newsData}/>} />
-      <Route exact path='/Article' render={() => <Article />}/>
+      <Switch>
+        <Route path='/' render={() => <NewsList newsData={newsData}/>} />
+        <Route path='/article' component={Article} />
+      </Switch>
     </div>
   );
 }
